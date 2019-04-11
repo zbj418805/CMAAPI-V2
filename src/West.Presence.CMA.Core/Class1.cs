82ab2d910b441c8669edaf2144d602697c8e0a4a
Jing Zhang <jzhang2@west.com>
2019-04-11 11:40:26 -0400

    Initi setup

---
+++ src/West.Presence.CMA.Core/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace West.Presence.CMA.Core
+{
+    public class Class1
+    {
+    }
+}
---
+++ test/West.Presence.CMA.Core.Integ.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace West.Presence.CMA.Core.Integ.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
