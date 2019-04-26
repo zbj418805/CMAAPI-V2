b380b06d69e7b039653e8d2dca79379e42e9a909
Jing Zhang <kingzad.zhang@gmail.com>
2019-04-25 23:04:05 -0400

    fix unit SchoolController

--- test/West.Presence.CMA.Api.Tests/Controllers/SchoolControllerTests.cs
+++ test/West.Presence.CMA.Api.Tests/Controllers/SchoolControllerTests.cs
@@ -1,27 +1,26 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Moq;
 using West.Presence.CMA.Api.Controllers;
+using West.Presence.CMA.Api.Model;
 using Xunit;
 
 namespace West.Presence.CMA.Api.Tests.Controllers
 {
     public class SchoolControllerTests
     {
-        //private SchoolsController _sut;
+        private SchoolsController _sut;
 
         [Fact]
-        public void Test_SchoolControll_Returns_OK()
+        public void Test_SchoolController_Returns_OK()
         {
-            /*
             // Arrange
-            _sut = new SchoolsController();
+            _sut = new SchoolsController(null);
             // Act
-            var result = _sut.GetSchools(null, null, null);
+            var result = _sut.GetSchools(null, new QueryFilter(), null,"");
 
             // Assert
-            Assert.IsType<OkResult>(result);
+            Assert.IsType<NoContentResult>(result);
             Assert.Equal(0, _sut.ModelState.ErrorCount);
-            */
         }
     }
 }
