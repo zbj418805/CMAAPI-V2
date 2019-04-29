7eb9569fee97ac07a65231a84a6d863d7b93192f
Jing Zhang <jzhang2@west.com>
2019-04-29 16:17:23 -0400

    add Channel2Group Path..

--- src/West.Presence.CMA.Core/Repositories/Channel2GroupRepository.cs
+++ src/West.Presence.CMA.Core/Repositories/Channel2GroupRepository.cs
@@ -20,17 +20,14 @@ namespace West.Presence.CMA.Core.Repositories
     public class DBChannel2GroupRepository : IChannel2GroupRepository
     {
         IDatabaseProvider _databaseProvider;
-
         public DBChannel2GroupRepository(IDatabaseProvider databaseProvider)
         {
             _databaseProvider=databaseProvider;
         }
-
         public IEnumerable<Channel2Group> GetChannel2Group(string baseUrl, int districtServerId)
         {
             return _databaseProvider.GetData<Channel2Group>("[dbo].[cma_c2g.get_all]", new { district_server_Id = districtServerId }, CommandType.StoredProcedure);
         }
-
         public void SetChannel2Group(string baseUrl, int districtServerId, List<Channel2Group> c2gs, int appId, string endpointUrl, string sessionId)
         {
             foreach(Channel2Group c2g in c2gs)
@@ -46,14 +43,12 @@ namespace West.Presence.CMA.Core.Repositories
                 sessionId = sessionId
             });
         }
-
         public int GetGroupId(string baseUrl, int serverId)
         {
             return _databaseProvider.GetCellValue<int>("SELECT TOP 1 shoutem_group_id FROM cma_entries WHERE server_id=@sId and content_type='news'",
                 new { sId = serverId },
                 CommandType.Text);
         }
-
         public AppExtended GetAppExtended(string baseUrl, int districtServerId)
         {
             return _databaseProvider.GetData<AppExtended>("SELECT app_id as appId, endpoint, session_id as sessionId, last_modified as lastModified, district_server_id as dictrictServerId FROM cma_extended WHERE district_server_id=@dsi ",
@@ -95,7 +90,7 @@ namespace West.Presence.CMA.Core.Repositories
 
         public int GetGroupId(string baseUrl, int serverId)
         {
-            return _httpClientProvider.GetData<int>(baseUrl + "asf/"+ serverId).FirstOrDefault();
+            return _httpClientProvider.GetData<int>(baseUrl + "webapi/cma/channel2group/" + serverId).FirstOrDefault();
         }
 
         public void SetChannel2Group(string baseUrl, int districtServerId, List<Channel2Group> c2gs, int appId, string endpointUrl, string sessionId)
@@ -107,7 +102,7 @@ namespace West.Presence.CMA.Core.Repositories
                 sessionId = sessionId
             };
 
-            _httpClientProvider.PostData<string>(baseUrl + "kdasd/fase/fasde", payload);
+            _httpClientProvider.PostData<string>(baseUrl + "webapi/cma/channel2group/", payload);
 
             throw new NotImplementedException();
         }
