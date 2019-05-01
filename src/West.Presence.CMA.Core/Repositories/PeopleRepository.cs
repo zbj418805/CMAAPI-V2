79e5fd1ee06192a4fae84946d652cb7eed362980
Jing Zhang <jzhang2@west.com>
2019-05-01 17:27:51 -0400

    Save wrok

--- src/West.Presence.CMA.Core/Repositories/PeopleRepository.cs
+++ src/West.Presence.CMA.Core/Repositories/PeopleRepository.cs
@@ -18,6 +18,67 @@ namespace West.Presence.CMA.Core.Repositories
 
         public IEnumerable<Person> GetPeople(int serverId, string baseUrl)
         {
+            /*
+            //1.Get portlet Instance id
+            int portletInstanceId = Utility.ToInteger(Database.ExecuteScalar("Mobile.Cma", "GetStaffDirectoryInstanceId", serverId));
+
+
+            //2.Get portlet Instance Properties to [selectedGroups], [excludedUsers](Using Presence Class)
+            PortletInstance ptlInstance = new PortletInstance(portletInstanceId);
+            if (!Utility.IsPositiveInteger(ptlInstance.PortletInstanceId))
+                return null;
+            List<int> selectedGroups = new List<int>();
+            List<int> excludedUsers = new List<int>();
+            XmlDocument doc = new XmlDocument();
+            if (ptlInstance.Properties.ContainsProperty("SelectedGroups"))
+            {
+                doc.LoadXml(ptlInstance.Properties["SelectedGroups"]);
+                XmlNode node = doc.SelectSingleNode("SelectedGroups");
+                if (Utility.ToBoolean(node.Attributes["visible"].Value))
+                {
+                    foreach (XmlNode groupNode in node.ChildNodes)
+                    {
+                        selectedGroups.Add(Utility.ToInteger(groupNode.Attributes["id"].Value));
+                    }
+                }
+            }
+            if (ptlInstance.Properties.ContainsProperty("ExcludedUsers"))
+            {
+                doc.LoadXml(ptlInstance.Properties["ExcludedUsers"]);
+                XmlNode node = doc.SelectSingleNode("ExcludedUsers");
+                foreach (XmlNode userNode in node.ChildNodes)
+                {
+                    excludedUsers.Add(Utility.ToInteger(userNode.Attributes["user_id"].Value));
+                }
+            }
+
+
+            //5.Get all users to [dtSimpleUsers] with [selectedGroups]
+            if (selectedGroups.Count == 0)
+                return null;
+            string groupIdsString = string.Join(",", selectedGroups.ToArray());
+            DataTable dtSimpleUsers = Database.Execute("Mobile.Cma", "GetSimpleUserInfoList", groupIdsString);
+
+            //6.Convert table [dtSimpleUsers] to list [spUsers] and remove users of [excludedUsers]
+            List<SimplePerson> spUsers = new List<SimplePerson>();
+            foreach (DataRow dr in dtSimpleUsers.Rows)
+            {
+                if (!excludedUsers.Contains(Utility.ToInteger(dr["user_id"])))
+                {
+                    spUsers.Add(new SimplePerson
+                    {
+                        userId = Utility.ToInteger(dr["user_id"]),
+                        firstName = Utility.ToString(dr["first_names"]),
+                        lastName = Utility.ToString(dr["last_name"]),
+                        jobTitle = Utility.ToString(dr["job_title"])
+                    });
+                }
+            }
+
+            //7.Return result [dtSimpleUsers]
+            return spUsers;
+            */
+
             throw new NotImplementedException();
         }
 
